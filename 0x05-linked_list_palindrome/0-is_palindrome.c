c1b8bb66eebc25df1e6cb174e8c2de1c1f89fcd5
giosermon <2172@holbertonschool.com>
2022-01-20 20:56:29 -0500

    add updates

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+
+int is_equal(listint_t **left, listint_t *right);
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: linked list to check
+ * Return: 1 if truo 0 otherwise
+ */
+int is_palindrome(listint_t **head)
+{
+	/* If list is empty, consider it a palindrome */
+	if (*head == NULL)
+		return (1);
+
+    /* Check if list is palindrome */
+    return is_equal(head, *head);
+}
+
+/**
+ * is_equal - Recursively check if symmetric nodes are of equal value
+ * @left: Node probing from the left
+ * @right: Node probing from the right
+ *
+ * Return: 1 if values match; otherwise, 0
+ */
+int is_equal(listint_t **left, listint_t *right)
+{
+	int is_palin = 0;
+
+	/* Base case is end of list */
+	if (right == NULL)
+		return (1);
+
+	/* Check if nodes have equal value */
+	if (!is_equal(left, right->next))
+		return (0);
+
+	/* Check is node pair is palindrome */
+	is_palin = (right->n == (*left)->n);
+
+    /* Move left pointer forward */
+	*left = (*left)->next;
+
+	return is_palin;
+}
