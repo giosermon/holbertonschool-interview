4ba4ba8221d7b81c5fc7b0eb4d1f1fd7129adac0
giosermon <2172@holbertonschool.com>
2022-01-27 21:58:27 -0500

    update

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -30,21 +30,22 @@ Counter = 0
 try:
     for line in stdin:
         lineSplit = line.split()
-
+        # Pull necessary fields from log line
         if len(lineSplit) >= 2:
             statusCode, fileSize = [part for part in lineSplit[-2:]]
 
+            # Update persistent size and status counters
             T_File_Size += int(fileSize)
             if statusCode in Tracker:
                 Tracker[statusCode] += 1
 
-
+                # Keep track of how many logs have been read in print loop
                 if Counter == 9:
                     printCodeTracking(T_File_Size, Tracker)
                     Counter = 0
                 else:
                     Counter += 1
-
+    # Print stats at end of input stream
     printCodeTracking(T_File_Size, Tracker)
 
 except KeyboardInterrupt:
