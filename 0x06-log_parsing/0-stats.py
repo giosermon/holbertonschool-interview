05ce257ab2a106e8da883f77a0031b549ffc825e
giosermon <2172@holbertonschool.com>
2022-02-21 22:10:33 -0500

    update

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,54 +1,39 @@
 #!/usr/bin/python3
 """ A program to ingest and track logs, periodically printing stats.
 """
+import sys
 
+if __name__ == "__main__":
 
-from sys import stdin, exit
+    status = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+              "404": 0, "405": 0, "500": 0}
+    size = 0
+    print10Lines = 0
 
+    try:
+        for text in sys.stdin:
+            code = text.split('"')[2].split(" ")[0]
+            unitSize = int(text.split('"')[2].split(" ")[1])
+            size += unitSize
+            print10Lines += 1
 
-def printCodeTracking(totalFileSize, codeTracker):
-    """ Print formatted log stats.
-    """
-    # Print total size of data passed to date
-    print('File size: ' + str(totalFileSize))
+            for key in sorted(status.keys()):
+                if code == key:
+                    status[key] += 1
 
-    codeList = sorted(codeTracker.keys())
+            if print10Lines == 10:
+                print("File size: {:d}".format(size))
+                for key in sorted(status.keys()):
+                    if status[key] and status is int:
+                        print("{}: {:d}".format(key, status[key]))
+                print10Lines = 0
 
-    # Print formatted count of requests by status code
-    for code in codeList:
-        if codeTracker[code] != 0:
-            print(code + ': ' + str(codeTracker[code]))
+    except KeyboardInterrupt:
+        pass
 
+    finally:
+        print("File size: {:d}".format(size))
 
-codeTracker = {
-    '200': 0, '301': 0, '400': 0, '401': 0,
-    '403': 0, '404': 0, '405': 0, '500': 0
-}
-totalFileSize = 0
-loopCounter = 0
-
-try:
-    for line in stdin:
-        # Pull necessary fields from log line
-        lineSplit = line.split()
-
-        if len(lineSplit) >= 2:
-            statusCode, fileSize = [part for part in lineSplit[-2:]]
-
-            # Update persistent size and status counters
-            totalFileSize += int(fileSize)
-            if statusCode in codeTracker:
-                codeTracker[statusCode] += 1
-
-                # Keep track of how many logs have been read in print loop
-                if loopCounter == 9:
-                    printCodeTracking(totalFileSize, codeTracker)
-                    loopCounter = 0
-                else:
-                    loopCounter += 1
-
-    # Print stats at end of input stream
-    printCodeTracking(totalFileSize, codeTracker)
-
-except KeyboardInterrupt:
-    printCodeTracking(totalFileSize, codeTracker)
+        for key in sorted(status.keys()):
+            if status[key] and status is int:
+                print("{}: {:d}".format(key, status[key]))
