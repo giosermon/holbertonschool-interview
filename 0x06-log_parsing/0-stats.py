72dbec1d5c45fd3242bec3d06b4dc6ef3f299a1f
giosermon <2172@holbertonschool.com>
2022-02-21 22:12:39 -0500

    update

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,39 +1,36 @@
 #!/usr/bin/python3
-""" A program to ingest and track logs, periodically printing stats.
 """
-import sys
+module for log parsing
+"""
+
+from sys import stdin
 
 if __name__ == "__main__":
-
-    status = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-              "404": 0, "405": 0, "500": 0}
-    size = 0
-    print10Lines = 0
-
+    total = 0
+    codes = {"200": 0, "301": 0, "400": 0,
+             "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
     try:
-        for text in sys.stdin:
-            code = text.split('"')[2].split(" ")[0]
-            unitSize = int(text.split('"')[2].split(" ")[1])
-            size += unitSize
-            print10Lines += 1
-
-            for key in sorted(status.keys()):
-                if code == key:
-                    status[key] += 1
-
-            if print10Lines == 10:
-                print("File size: {:d}".format(size))
-                for key in sorted(status.keys()):
-                    if status[key] and status is int:
-                        print("{}: {:d}".format(key, status[key]))
-                print10Lines = 0
-
+        for count, line in enumerate(stdin):
+            args = line.split(" ")
+            if len(args) < 2:
+                continue
+            if args[-2] in codes:
+                codes[args[-2]] += 1
+            try:
+                total += int(args[-1])
+            except:
+                pass
+            if (count + 1) % 10 == 0:
+                print("File size: {}".format(total))
+                for status in sorted(codes.keys()):
+                    if codes[status] != 0:
+                        print("{}: {}".format(status, codes[status]))
+        print("File size: {}".format(total))
+        for status in sorted(codes.keys()):
+            if codes[status] != 0:
+                print("{}: {}".format(status, codes[status]))
     except KeyboardInterrupt:
-        pass
-
-    finally:
-        print("File size: {:d}".format(size))
-
-        for key in sorted(status.keys()):
-            if status[key] and status is int:
-                print("{}: {:d}".format(key, status[key]))
+        print("File size: {}".format(total))
+        for status in sorted(codes.keys()):
+            if codes[status] != 0:
+                print("{}: {}".format(status, codes[status]))
