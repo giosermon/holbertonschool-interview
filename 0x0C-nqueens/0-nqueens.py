4dd03bf21ff774044c72e91ee1c9086f720a2307
giosermon <2172@holbertonschool.com>
2022-03-29 20:54:01 -0500

    update

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -7,32 +7,60 @@ Module to solve the N Queens Problem
 import sys
 
 
-if __name__ == "__main__":
+def queens(n):
+    """program that solves the N queens problem"""
+    trail = []
+    sets = set()
+    for column in range(n):
+        trail.append([0, column])
+        sets.add(column)
+
+    road = []
+    while trail:
+        [row, column] = trail.pop(0)
+        while road and (row < road[0][0]):
+            road.pop(0)
+        if road and (row == road[0][0]):
+            road[0] = [row, column]
+        else:
+            road.insert(0, [row, column])
+
+        nextsrows = row + 1
+        death = set()
+        for (i, j) in road:
+            death.add(j)
+            distance = nextsrows - i
+            if j - distance >= 0:
+                death.add(j - distance)
+            if j + distance < n:
+                death.add(j + distance)
+
+        safe = sets.difference(death)
+        if not safe:
+            if nextsrows == n:
+                temp = road.copy()
+                temp.reverse()
+                print(temp, flush=True)
+            road.pop(0)
+        else:
+            safe = list(safe)
+            safe.reverse()
+            for position in safe:
+                trail.insert(0, [nextsrows, position])
+
+
+if __name__ == '__main__':
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
-        sys.exit(1)
+        exit(1)
+
+    if int(sys.argv[1]) < 4:
+        print("N must be at least 4")
+        exit(1)
 
     try:
-        N = int(sys.argv[1])
-    except ValueError:
+        n = int(sys.argv[1])
+    except:
         print("N must be a number")
-        sys.exit(1)
-
-    if N < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-
-
-def backtracking(N, i=0, a=[], b=[], c=[]):
-    """Generates backtracking solutions for N queens problem"""
-    if i < N:
-        for j in range(N):
-            if j not in a and i+j not in b and i-j not in c:
-                for solution in backtracking(N, i+1, a+[j], b+[i+j], c+[i-j]):
-                    yield solution
-    else:
-        yield a
-
-for solution in backtracking(N):
-    answer = [[col, row] for col, row in enumerate(solution)]
-    print(answer)
+        exit(1)
+    queens(n)
